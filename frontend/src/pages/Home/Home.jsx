5369553e130ddfddcf9e8cf196ffd73861df3a6d
Sahil Mishra <sahilmishra.btechcs24115@iecgn.in>
2026-07-09 20:57:34 +0530

    add componants and some functionality

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React , { useState }  from 'react'
+
+import './Home.css'
+import Header from '../../componants/Header/Header.jsx'
+import ExploreMenu from '../../componants/ExploreMenu/ExploreMenu.jsx'
+import FoodDisplay from '../../componants/FoodDisplay/FoodDisplay.jsx'
+
+const Home = () => {
+
+  const [category , setCategory] = useState('All');
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+    </div>
+  )
+}
+
+export default Home
