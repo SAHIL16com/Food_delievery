87e548a08ae09b012921d11f3888da073b7fea26
Sahil Mishra <sahilmishra.btechcs24115@iecgn.in>
2026-07-10 11:48:55 +0530

    add context for cart items

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,35 @@
 import { createContext } from "react";
 import { food_list } from '../assets/assets.js';
 export const StoreContext = createContext(null)
+import { useState } from "react";
+import { useEffect } from "react";
 
 const StoreContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems(prev => ({ ...prev, [itemId]: 1 }));
+        }
+        else {
+            setCartItems(prev => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        }
+
+    }
+    const removeFromCart = (itemId) => {
+        setCartItems(prev => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
 
 
     const ContextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
     }
 
     return (
