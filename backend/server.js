d861ec087cdd2144e6d83739233f25843c39cead
Sahil Mishra <sahilmishra.btechcs24115@iecgn.in>
2026-07-11 13:02:23 +0530

    add mongodb and controllers and routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
 
 //app. config
 const app = express();
@@ -10,9 +11,12 @@ const port = 4000;
 connectDB();
 
 //middleware
-
 app.use(express.json());
 app.use(cors());
+
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
 
 app.get('/', (req , res) =>{
     res.send("the api is working")
