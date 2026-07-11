07eb226e1a05b380687e2a37e461c2df839af7b2
Sahil Mishra <sahilmishra.btechcs24115@iecgn.in>
2026-07-11 09:35:53 +0530

    add cart in webpage

--- frontend/src/componants/Navbar/Navbar.jsx
+++ frontend/src/componants/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets.js'
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({ setShowLogin }) => {
-
   const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className='logo' />
+      <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
       <ul className='menu-items'>
         <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
         <a href ='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
@@ -19,12 +19,11 @@ const Navbar = ({ setShowLogin }) => {
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div>
-          <img src={assets.cart_icon} alt="" />
-          <div className="navbar-search-icon">
+        <div className="navbar-basket-container">
+          <Link to='/cart' className="navbar-search-icon">
             <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
-          </div>
+            {getTotalCartAmount() > 0 && <div className="dot"></div>}
+          </Link>
           <button onClick={() => setShowLogin(true)} className='sign-in-btn'>Sign in</button>
         </div>
       </div>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -19,6 +19,18 @@ const StoreContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems(prev => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                if (itemInfo) {
+                    totalAmount += itemInfo.price * cartItems[item];
+                }
+            }
+        }
+        return totalAmount;
+    }
     useEffect(() => {
         console.log(cartItems);
     }, [cartItems])
@@ -30,6 +42,7 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
+        getTotalCartAmount,
     }
 
     return (
